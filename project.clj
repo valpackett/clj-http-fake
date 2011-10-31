c11961ef50d262dfe1e6feb2063e24e2a7e403d8
myfreeweb <me@myfreeweb.ru>
2011-10-31 23:15:54 +0400

    Too simple for autodoc

--- project.clj
+++ project.clj
@@ -4,15 +4,6 @@
   :license {:name "MIT License"
             :url  "http://www.opensource.org/licenses/mit-license.php"
             :distribution :repo}
-  :autodoc {:name "clj-http-fake"
-            :page-title "clj-http-fake API docs"
-            :web-src-dir "https://github.com/myfreeweb/clj-http-fake/blob/"
-            :web-home "http://myfreeweb.github.com/clj-http-fake/"
-            :output-path "autodoc"
-            :root "."
-            :source-path ""
-            :load-except-list [#"test/" #"project\.clj"]}
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [clj-http "0.2.3"]
-                 [robert/hooke "1.1.2"]]
-  :dev-dependencies [[org.clojars.weavejester/autodoc "0.9.0"]])
+                 [robert/hooke "1.1.2"]])
